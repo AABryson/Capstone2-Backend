608882d1c933e62f9ef75d3f0a52cb8df83148c9
AABryson <144609457+AABryson@users.noreply.github.com>
2024-07-09 14:21:17 -0700

    have not finished login

--- rusers.js
+++ rusers.js
@@ -14,9 +14,21 @@ router.post('/signup', async (req, res, next) => {
     }
 })
 
-router.post
+router.post('/login', async (req, res, next) => {
+    try {
+        const {username, password} = req.body;
+        console.log('is username and password in req.boy', req.body)
+        if (!password) {
+            return res.status(400).send({error: 'Password is required'})
+        }
+        let user = await Users.userLogin(username, password)
+        res.status(201).json({user})
+    } catch(err) {
+        return next(err)
+    }
+})
 
-module.exports=router
+module.exports = router
 
 // router.post('./login', (req, res, next){
 //     let {usernme, email} = req.body
